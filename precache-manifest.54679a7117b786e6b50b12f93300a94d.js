cbc85383f9f564f7b0ad53ba94f98f528b755ad7
uratomo <rop.ibuki@gmail.com>
2020-10-05 17:16:20 +0900

    Updates

---
+++ precache-manifest.54679a7117b786e6b50b12f93300a94d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76a48130d48c51d13a7e87e5217f12e",
+    "url": "/react_miniapp/index.html"
+  },
+  {
+    "revision": "3f9b4179d63da8c41192",
+    "url": "/react_miniapp/static/css/main.71a55c6a.chunk.css"
+  },
+  {
+    "revision": "dc46c84b29e88f96eb08",
+    "url": "/react_miniapp/static/js/2.1a2b90b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_miniapp/static/js/2.1a2b90b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f9b4179d63da8c41192",
+    "url": "/react_miniapp/static/js/main.81eda324.chunk.js"
+  },
+  {
+    "revision": "d13eac8c4cf6f68e0c9b",
+    "url": "/react_miniapp/static/js/runtime-main.51c27b17.js"
+  }
+]);
